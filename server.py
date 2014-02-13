e55938fe9f6dd0af45c1893c8ab6529f62bc2659
Caitlin McDonald <caitlin.adele.mcdonald@gmail.com>
2014-02-12 23:37:51 -0500

    Project 5 hurts my soul

--- server.py
+++ server.py
@@ -3,9 +3,8 @@ import random
 import socket
 import time
 from urlparse import urlparse, parse_qs
-from jinja2 import FileSystemLoader, Environment
-from cgi import FieldStorage
 from StringIO import StringIO
+from app import make_app
 
 def handle_connection(conn):
 
@@ -27,71 +26,45 @@ def handle_connection(conn):
         key, value = line.split(':', 1)
         header_information[key] = value
 
-    # Split into thre pieces of information:
+    # Split into three pieces of information:
     # HTTP method -- path -- HTTP version
     request = request.split(' ', 2)
 
-
     path_information = urlparse(request[1])
 
-    # Link paths to their associated html pages
-    paths = {
-        '/'        : 'index.html',   \
-        '/content' : 'content.html', \
-        '/file'    : 'file.html',    \
-        '/image'   : 'image.html',   \
-        '/form'    : 'form.html',    \
-        '/submit'  : 'submit.html',  \
-        }
+    # Build the environ object
+    environ = {}
+    path = urlparse(req.split(' ', 3)[1])
+    environ['REQUEST_METHOD'] = 'GET'
+    environ['PATH_INFO'] = path.path
+    environ['QUERY_STRING'] = path.query
+    environ['CONTENT_TYPE'] = 'text/html'
+    environ['CONTENT_LENGTH'] = 0
 
-    # Load templates & prepare basic HTML
-    loader = FileSystemLoader('./templates')
-    env = Environment(loader=loader)
-    meta =  "HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n"
+    def start_response(status, response_headers):
+        conn.send('HTTP/1.0')
+        conn.send(status)
+        conn.send('\r\n')
+        for pair in response_headers:
+            key, value = pair
+            conn.send(key + ": " +  value + '\r\n')
+        conn.send('\r\n')
+
     content = ''
-
-    # Grab query information from GET request
-    data = parse_qs(path_information.query)
-    # Query info is stored in an odd way
-    # Change the values from ['value'] to 'value'
-    for key, value in data.items():
-        data[key] = data[key][0]
-
-    # Extra handling for POST request
-    if request[0] == 'POST':
-        # Continue to gather the content body
+    if req.startswith('POST '):
+        environ['REQUEST_METHOD'] = 'POST'
+        environ['CONTENT_LENGTH'] = header_information['Content-Length']
+        environ['CONTENT_TYPE'] = header_information['Content-Type']
         while len(content) < int(header_information['Content-Length']):
             content += conn.recv(1)
 
-        # Using CGI.FieldStorage to support multipart/form-data
-        fs= FieldStorage(
-            fp=StringIO(content),               \
-            headers=header_information,         \
-            environ={'REQUEST_METHOD' : 'POST'} \
-            )
+    environ['wsgi.input'] = StringIO(content)
+    application = make_app()
+    result = application(environ, start_response)
+    
+    for data in result:
+        conn.send(data)
 
-        # Take parsed information and add it to our data dictionary
-        additional_data = {}
-        for item in fs.keys():
-            additional_data[item] = fs[item].value
-        data.update(additional_data)
-
-    # Validate requested path
-    if path_information.path in paths:
-        template = env.get_template(paths[path_information.path])
-
-    # Send to 404 if not a valid path
-    else:
-        meta = "HTTP/1.0 404 Not Found\r\n\r\n"
-        data['path'] = path_information.path
-        template = env.get_template('404.html')
-        
-
-    # Build our webpage
-    webpage = meta + template.render(data)
-
-    # Send that puppy
-    conn.send(webpage)
     conn.close()
 
 def main():
